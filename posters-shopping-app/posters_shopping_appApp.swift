18db91fa64ba902f9dfd45fa6d4d60e3e14c42a8
Raul Pham <phatpham314@icloud.com>
2023-04-26 10:52:59 -0500

    Initial Commit

---
+++ posters-shopping-app/posters_shopping_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  posters_shopping_appApp.swift
+//  posters-shopping-app
+//
+//  Created by Pham on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct posters_shopping_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
